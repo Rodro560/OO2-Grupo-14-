d6ca81483b30d6b68665f6bcfa8103770a335c54
Rodro <Rodro@DESKTOP-6CLD8V8>
2025-05-06 16:59:38 -0300

    Agrego a servicioDao

---
+++ Tp grupo 14-TurnosHibernate/src/dao/ServicioDao.java
@@ -0,0 +1,108 @@
+package dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.Hibernate;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import datos.Servicio;
+
+public class ServicioDao {
+	
+	private static Session session;
+	private Transaction tx;
+	
+	private void iniciaOperacion() throws HibernateException{
+		session= HibernateUtil.getSessionFactory().openSession();
+		tx= session.beginTransaction();
+	}
+	
+	private void manejaExcepcion(HibernateException he) throws HibernateException{
+		tx.rollback();
+		throw new HibernateException("ERROR en la capa de acceso a datos", he);
+	}
+	
+	
+	public int agregar(Servicio objeto) {
+		int id=0;
+		try {
+			iniciaOperacion();
+			id= Integer.parseInt(session.save(objeto).toString());
+			tx.commit();
+		}catch (HibernateException he) {
+			manejaExcepcion(he);
+		}finally {
+			session.close();
+		}
+		return id;
+	}
+	
+	public void actualizar(Servicio objeto) {
+		try {
+			iniciaOperacion();
+			session.update(objeto);
+			tx.commit();
+		} catch (HibernateException he) {
+			manejaExcepcion(he);
+		}finally {
+			session.close();
+		}
+	}
+	
+	public void eliminar(Servicio objeto) {
+		try {
+			iniciaOperacion();
+			session.delete(objeto);
+			tx.commit();
+		} catch (HibernateException he) {
+			manejaExcepcion(he);
+		}finally {
+			session.close();
+		}
+	}
+	
+	public Servicio traer(long idServicio) {
+		Servicio objeto=null;
+		try {
+			iniciaOperacion();
+			objeto= (Servicio) session.get(Servicio.class, idServicio);
+		} finally {
+			session.close();
+		} 
+		return objeto;
+	}
+	
+	public List<Servicio> traerServiciosPorPrestador(int idPrestador) {
+	    List<Servicio> lista = new ArrayList<>();
+	    try {
+	        iniciaOperacion();
+	        String hql = "FROM Servicio s WHERE s.prestador.id = :idPrestador";
+	        Query<Servicio> query = session.createQuery(hql, Servicio.class);
+	        query.setParameter("idPrestador", idPrestador);
+	        lista = query.list();
+	    } finally {
+	        session.close();
+	    }
+	    return lista;
+	}
+	
+	public List<Servicio> traerTodos() {
+	    List<Servicio> lista = new ArrayList<>();
+	    try {
+	        iniciaOperacion();
+	        String hql = "FROM Servicio";
+	        Query<Servicio> query = session.createQuery(hql, Servicio.class);
+	        lista = query.list();
+	    } catch (HibernateException he) {
+	        manejaExcepcion(he);
+	    } finally {
+	        session.close();
+	    }
+	    return lista;
+	}
+
+}
