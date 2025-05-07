bf0ff91966cfb10b4e16099858ebdfc0c04add25
Rodro <Rodro@DESKTOP-6CLD8V8>
2025-05-07 20:47:28 -0300

    correcion de el test servicio

--- Tp grupo 14-TurnosHibernate/src/test/testServicioAbm.java
+++ Tp grupo 14-TurnosHibernate/src/test/testServicioAbm.java
@@ -1,7 +1,11 @@
 package test;
 
+import java.util.List;
+
+import datos.Prestador;
 import datos.Servicio;
 import negocio.ServicioABM;
+import negocio.PrestadorABM;
 
 public class testServicioAbm {
 
@@ -9,11 +13,13 @@ public class testServicioAbm {
 		// TODO Auto-generated method stub
 		
 		ServicioABM servicio= new ServicioABM();
+		PrestadorABM prestador= new PrestadorABM();
 		
 		try {
-			servicio.crearServicio("Servicio 1", "Es el servicio 1", 0, 10.50f, null, null);
-			servicio.crearServicio("Servicio 2", "Es el servicio 2", 0, 10.50f, null, null);
-			servicio.crearServicio("Servicio 3", "Es el servicio 3", 0, 10.50f, null, null);
+			servicio.crearServicio("Servicio 1", "Es el servicio 1", 0, 10.50f, prestador.traerPrestador("buenas"), null);
+			servicio.crearServicio("Servicio 2", "Es el servicio 2", 0, 10.50f, prestador.traerPrestador("buenas"), null);
+			servicio.crearServicio("Servicio 3", "Es el servicio 3", 0, 10.50f, prestador.traerPrestador("ghrg"), null);
+			servicio.crearServicio("Servicio 4", "Es el servicio 4", 0, 10.50f, prestador.traerPrestador("buenas"), null);
 			servicio.eliminarServicio(servicio.traerServicio("Servicio 2").getIdServicio());
 		} catch (Exception e) {
 			// TODO: handle exception
@@ -26,6 +32,19 @@ public class testServicioAbm {
 			servicio.modificarServicio(s);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try {
+			
+			Prestador p = prestador.traerPrestador("buenas");
+		
+			List<Servicio> serviciosDelPrestador = servicio.traerServiciosPorPrestador(p);
+
+			for (Servicio s : serviciosDelPrestador) {
+				System.out.println("Servicio: " + s.getNombre());
+			}
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		
