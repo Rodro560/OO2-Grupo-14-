304051cfc94ab07cac28374b3850ba099847fd55
Bono-Dev929 <bono.dev.22@gmail.com>
2025-05-08 11:40:05 -0300

    Update bd_sistema_turnos.sql
    Atributo sobre servicio en disponibilidad eliminado por duplicacion de datos

--- Tp grupo 14-TurnosHibernate/bd_sistema_turnos.sql
+++ Tp grupo 14-TurnosHibernate/bd_sistema_turnos.sql
@@ -47,10 +47,7 @@ CREATE TABLE `disponibilidad` (
   `diaSemana` enum('LUNES','MARTES','MIERCOLES','JUEVES','VIERNES','SABADO','DOMINGO') NOT NULL,
   `horaInicio` time NOT NULL,
   `horaFin` time NOT NULL,
-  `servicio_id` int NOT NULL,
   PRIMARY KEY (`idDisponibilidad`),
-  KEY `servicio_id` (`servicio_id`),
-  CONSTRAINT `disponibilidad_ibfk_1` FOREIGN KEY (`servicio_id`) REFERENCES `servicio` (`idServicio`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
