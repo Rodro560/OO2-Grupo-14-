5e53e8e74351db92f07af19007b4379f54678381
Luca Fattorini <117039305+lucafattorinii@users.noreply.github.com>
2025-05-08 12:39:35 -0300

    Corrección y mejoras en la clase Cliente.java

--- Tp grupo 14-TurnosHibernate/src/datos/Cliente.java
+++ Tp grupo 14-TurnosHibernate/src/datos/Cliente.java
@@ -4,58 +4,57 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Cliente extends Usuario {
-	
-	private long dni;
-	private String nombre;
-	private String apellido;
-	private Set<Turno> lstTurnos;
-	
-	public Cliente() {}
 
-	public Cliente( String email, String contrasenia, long dni, String nombre, String apellido) {
-		super( email, contrasenia);
-		this.dni = dni;
-		this.nombre = nombre;
-		this.apellido = apellido;
-		this.lstTurnos = new HashSet<Turno>();
-	}
+    private long dni;
+    private String nombre;
+    private String apellido;
+    private Set<Turno> lstTurnos;
 
-	public long getDni() {
-		return dni;
-	}
+    public Cliente() {}
 
-	public void setDni(long dni) {
-		this.dni = dni;
-	}
+    // Constructor
+    public Cliente(String email, String contrasenia, long dni, String nombre, String apellido) {
+        super(email, contrasenia); 
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.lstTurnos = new HashSet<>();
+    }
 
-	public String getNombre() {
-		return nombre;
-	}
+    public long getDni() {
+        return dni;
+    }
 
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
+    public void setDni(long dni) {
+        this.dni = dni;
+    }
 
-	public String getApellido() {
-		return apellido;
-	}
+    public String getNombre() {
+        return nombre;
+    }
 
-	public void setApellido(String apellido) {
-		this.apellido = apellido;
-	}
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 
-	public Set<Turno> getLstTurnos() {
-		return lstTurnos;
-	}
+    public String getApellido() {
+        return apellido;
+    }
 
-	public void setLstTurnos(Set<Turno> lstTurnos) {
-		this.lstTurnos = lstTurnos;
-	}
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
 
-	@Override
-	public String toString() {
-		return "Cliente ["+super.toString()+"dni=" + dni + ", nombre=" + nombre + ", apellido=" + apellido + "]";
-	}
-	
-	
+    public Set<Turno> getLstTurnos() {
+        return lstTurnos;
+    }
+
+    public void setLstTurnos(Set<Turno> lstTurnos) {
+        this.lstTurnos = lstTurnos;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente [" + super.toString() + ", dni=" + dni + ", nombre=" + nombre + ", apellido=" + apellido + "]";
+    }
 }
