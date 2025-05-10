04521d3a056a7a013088a9699b6d0fa36597854a
Rodro <Rodro@DESKTOP-6CLD8V8>
2025-05-10 19:00:03 -0300

    Agrego una excepcion al creaEspecificacion

--- Tp grupo 14-TurnosHibernate/src/negocio/EspecificacionABM.java
+++ Tp grupo 14-TurnosHibernate/src/negocio/EspecificacionABM.java
@@ -15,23 +15,32 @@ public class EspecificacionABM {
 	
 	
 	
-	public int crearEspecificacion(EnumRubro rubro, String detalles, String personalInvolucrado) throws Exception {
-		
-		if(rubro == null) {
-			throw new Exception("\nEl campo rubro esta vacio");
-		}
-		
-		if(detalles == null) {
-			throw new Exception("\nEl campo detalles esta vacio");
-		}
-		
-		if(personalInvolucrado == null) {
-			throw new Exception("\nEl campo personal involcrado esta vacio");
-		}
-		
-		Especificacion e= new Especificacion(rubro, detalles, personalInvolucrado);
-		
-		return dao.agregar(e);
+	public int crearEspecificacion(EnumRubro rubro, String detalles, String personalInvolucrado, Servicio servicio) throws Exception {
+	    
+	    if(rubro == null) {
+	        throw new Exception("\nEl campo rubro está vacío");
+	    }
+
+	    if(detalles == null) {
+	        throw new Exception("\nEl campo detalles está vacío");
+	    }
+
+	    if(personalInvolucrado == null) {
+	        throw new Exception("\nEl campo personal involucrado está vacío");
+	    }
+	    
+	    if(servicio == null) {
+	    	throw new Exception("\nEl campo servicio está vacío");
+	    }
+
+	    // Verifica si ya existe una especificación igual
+	    Especificacion existente = dao.traerEspecificacionExistente(rubro, detalles, personalInvolucrado, servicio);
+	    if (existente != null) {
+	        throw new Exception("\nYa existe una especificación con estos datos.");
+	    }
+
+	    Especificacion e = new Especificacion(rubro, detalles, personalInvolucrado, servicio);
+	    return dao.agregar(e);
 	}
 	
 	
